d222b948e214baa97a9f5161c90a5c2e7355a772
Shannon Huggins <shannon@techequipt.com.au>
2023-07-27 17:47:03 +1000

    hardcoded url for testing

--- src/wagtail_2fa/views.py
+++ src/wagtail_2fa/views.py
@@ -57,21 +57,19 @@ class LoginView(RedirectURLMixin, FormView):
 
     def get_redirect_url(self):
         """Return the user-originating redirect URL if it's safe."""
-        # redirect_to = self.request.POST.get(
-        #     self.redirect_field_name, self.request.GET.get(self.redirect_field_name, "")
-        # )
-        redirect_to= "/admin/"
-        if 'next' in self.request.GET:
-            redirect_to = self.request.GET.get('next')
-        # url_is_safe = url_has_allowed_host_and_scheme(
-        #     url=redirect_to,
-        #     allowed_hosts=self.get_success_url_allowed_hosts(),
-        #     require_https=self.request.is_secure(),
-        # )
-        return redirect_to
+        redirect_to = self.request.POST.get(
+            self.redirect_field_name, self.request.GET.get(self.redirect_field_name, "")
+        )
+        url_is_safe = url_has_allowed_host_and_scheme(
+            url=redirect_to,
+            allowed_hosts=self.get_success_url_allowed_hosts(),
+            require_https=self.request.is_secure(),
+        )
+        return redirect_to if url_is_safe else ""
 
     def get_success_url(self):
-        url = self.get_redirect_url()
+        #url = self.get_redirect_url()
+        url = "/cms/"
         return url or resolve_url(settings.LOGIN_REDIRECT_URL)
 
 
