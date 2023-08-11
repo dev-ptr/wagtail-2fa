43d89929e0493dd79729b1c34d18e5301c65a009
Shannon Huggins <phate@houseofsyn.com>
2023-08-11 16:42:57 +1000

    Removed hardcoded template testing

--- src/wagtail_2fa/views.py
+++ src/wagtail_2fa/views.py
@@ -29,7 +29,11 @@ from wagtail_2fa.mixins import OtpRequiredMixin
 
 
 class LoginView(RedirectURLMixin, FormView):
-    template_name = "wagtail_2fa/otp_form.html"
+    if WAGTAIL_VERSION >= (4, 0, 0):
+        template_name = "wagtail_2fa/otp_form.html"
+    else:
+        template_name = "wagtail_2fa/legacy/otp_form.html"
+
     form_class = forms.TokenForm
     redirect_field_name = REDIRECT_FIELD_NAME
 
